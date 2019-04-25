7c6f5b7bf7521f9e032fe221d7c8751505a7ca89
TheEquus <ghostgone67@gmail.com>
2019-04-25 14:25:26 +1000

    Update half_and_half.py

--- Crypto/half_and_half.py
+++ Crypto/half_and_half.py
@@ -13,9 +13,3 @@ milk = flag[:half]
 cream = flag[half:]
 
 assert xor(milk, cream) == '\x15\x02\x07\x12\x1e\x100\x01\t\n\x01"'
-
-hex codes = ['15', '02', '07', '12', '1e', '10', '30' '01', '09','0a', '01']
-milk = "actf{"
-cream = "taste"
-actf{coffee_
-tastes_off}
